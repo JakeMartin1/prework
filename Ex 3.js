89c98713cd7287d0180cc4bf1edc34c421168579
JakeMartin1 <113862958+JakeMartin1@users.noreply.github.com>
2022-10-02 17:41:35 -0500

    Ex 3
    Complete

---
+++ Ex 3.js
@@ -0,0 +1,10 @@
+console.log("This is a string, it has quotes.");
+console.log(100);
+console.log("100");
+
+console.log("My name is: ");
+console.log("Jake"); //change this line
+console.log("My age is: ");
+console.log(32); //change this line, which type should you use?
+console.log("My zip code is: "); 
+console.log("32507"); //change this line, which type should you use?
