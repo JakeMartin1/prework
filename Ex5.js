ecfb4dc038e8672bace73082eedc6c146349fd17
JakeMartin1 <113862958+JakeMartin1@users.noreply.github.com>
2022-10-03 16:44:44 -0500

    Ex5

---
+++ Ex5.js
@@ -0,0 +1,19 @@
+//Ex5
+console.log("True or false: 5 is less than 2");
+console.log(5 < 2);
+console.log("True or false: 19 is not equal 11");
+console.log(19 != 11)
+console.log("True or false: 10 is greater than or equal to 10");
+console.log(10 >= 10);
+
+//Ex5.2
+console.log(" ");
+console.log("Ex5.2");
+console.log("2=2")
+console.log(2==2); // ==, true
+console.log("2>4")
+console.log(2>4); // >, false
+console.log("2<=1")
+console.log(2<=1); // <=, false
+console.log("3000!=3001")
+console.log(3000!=3001); // !=, true
